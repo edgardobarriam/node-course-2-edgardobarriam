d49763a3e428a16d4a84bed529edfa9b3d44f632
Edgardo Barria Melian <edgardo.barriam@gmail.com>
2018-05-19 18:47:49 -0400

    retomar curso

--- 08-chat-app/server/server.js
+++ 08-chat-app/server/server.js
@@ -1,47 +1,11 @@
 const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
-const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const express = require('express')
+const PUBLIC_PATH = path.join(__dirname, '../public');
+const PORT = process.env.PORT || 3000
+
 var app = express();
-var server =http.createServer(app);
-var io = socketIO(server);
-app.use(express.static(publicPath));
+app.use(express.static(PUBLIC_PATH));
 
-io.on('connection', (socket) => {
-  console.log('new user connected');
-
-  // socket emit from admin text welcome
-  socket.on('newUserJoin', (message) => {
-    socket.emit('welcomeUser', {
-      text: 'Welcome new user'
-    });
-    socket.broadcast.emit('createUserNotification', {
-      text: 'A new user joined!'
-    });
-  });
-  // socket broadcast from admin  new user joined
-  socket.on('createMessage', (message) => {
-    console.log('create message ', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-
-  /*   socket.broadcast.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    }); */
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected');
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-server.listen(port,() => {
-  console.log(`Server started on port ${port}`);
-})
