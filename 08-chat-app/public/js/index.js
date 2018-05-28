5a2e80e1f60808b84247a557f1e8a87f7efb8cad
Edgardo Barria Melian <edgardo.barriam@gmail.com>
2018-05-28 18:50:57 -0400

    Add form for messages and show incoming messages in browser

--- 08-chat-app/public/js/index.js
+++ 08-chat-app/public/js/index.js
@@ -1,14 +1,30 @@
 var socket = io();
 
 socket.on('connect', function()  {
-  alert('connected to server');
-
+  console.log('connected to server');
 });
 
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
   alert('disconnected from server');
 });
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function() {
+    // Callback
+
+  });
+});
+
