4c09bba8baa24653318f4e869c9b703fc8ea207f
Edgardo Barria Melian <edgardo.barriam@gmail.com>
2018-08-29 20:59:49 -0300

    Add Messages autoscroll

--- 08-chat-app/public/js/index.js
+++ 08-chat-app/public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function autoScrollChat() {
+  // Selectors
+  var messages = $('#messages');
+  var newMessage  = messages.children('li:last-child');
+
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function()  {
   console.log('Connected to server');
 });
@@ -15,7 +32,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
-
+  autoScrollChat();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -29,6 +46,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
+  autoScrollChat();
 });
 
 socket.on('disconnect', function() {
