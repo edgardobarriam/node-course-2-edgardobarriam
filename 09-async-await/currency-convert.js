4ff7e9e7a41af795e51e1d0d97921b32830ad78b
Edgardo Barria Melian <edgardo.barriam@gmail.com>
2018-09-10 18:44:45 -0300

    Handling errors on async/await functions

--- 09-async-await/currency-convert.js
+++ 09-async-await/currency-convert.js
@@ -1,40 +1,49 @@
 const axios = require('axios');
-// USD, CAD, 20
-// 20 USD is worth 26 CAD. You can spend these in the following countries: Canada,...
-
 // http://data.fixer.io/api/latest?access_key=9355749bf4c76125cf9679bc5dbad9b8
 
-/* const getExchangeRate = (from, to) => {
-  return axios.get('http://data.fixer.io/api/latest?access_key=9355749bf4c76125cf9679bc5dbad9b8').then((response) => {
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=9355749bf4c76125cf9679bc5dbad9b8');
     const euro = 1 / response.data.rates[from];
     const rate = euro * response.data.rates[to];
-    
-    return rate;
-  });
-} */
 
-const getExchangeRate = async (from, to) => {
-  const response = await axios.get('http://data.fixer.io/api/latest?access_key=9355749bf4c76125cf9679bc5dbad9b8');
-  const euro = 1 / response.data.rates[from];
-  const rate = euro * response.data.rates[to];
-  return rate;
+    if (isNaN(rate)) {
+      throw new Error();
+    }
+
+    return rate;
+  } catch (error) {
+    throw new Error(`Unable to get echange rate for ${from} and ${to}.`);
+  }
 } 
 
-/* const getCountries = (currencyCode) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
-    return response.data.map((country) => country.name);
-  });
-} */
-
 const getCountries = async (currencyCode) => {
-  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-  return response.data.map((country) => country.name);
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    return response.data.map((country) => country.name);
+  } catch (error) {
+    throw new Error(`Unable to get countries that use ${currencyCode}.`);
+  }
 }
 
-getExchangeRate('USD', 'CAD').then((rate) => {
-  console.log(rate);
-});
+const convertCurrency = async (from, to, amount) => {
+  const rate = await getExchangeRate(from, to);
+  const convertedAmount = (rate * amount).toFixed(2);
+  const countries = await getCountries(to);
 
-getCountries('CAD').then((countries) => {
-  console.log(countries);
+  return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend these in the following countries: ${countries.join(', ')}`;
+} 
+
+convertCurrency('USD', 'CAD', 20)
+  .then( (message) => console.log(message) )
+  .catch( (error) => {console.log(error.message)} );
+
+const doWork = async () => {
+  return assad;
+}
+
+doWork().then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.log('Something went wrong');
 });
